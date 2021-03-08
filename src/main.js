766d36dbf171aa8d31d4724b18220359772b5e1d
Rafal Szczotka <rafalszczotka@wp.pl>
2021-03-09 00:24:02 +0100

    Added global styles. Added bootstrap to project, updated main.js. Added routers.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/index.js'
+
+import 'bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import axios from 'axios';
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8080/';
 
 createApp(App).use(store).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import TheLogin from '../views/TheLogin.vue'
+import OrderContent from '../views/orders/OrderContent.vue'
+import CompaniesList from '../views/merhants/CompaniesList.vue'
+import CompanyDetail from '../views/merhants/CompanyDetail.vue'
+import CompanyEdit from '../views/merhants/CompanyEdit.vue'
+import CompanyAdd from '../views/merhants/CompanyAdd.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'TheLogin',
+    component: TheLogin
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/orders',
+    name: 'OrderContent',
+    component: OrderContent
+  },
+  {
+    path: '/companies',
+    component: CompaniesList
+  },
+  {
+    path: '/company/:id',
+    component: CompanyDetail,
+    props: true,
+    children: [
+      {
+        path: 'edit',
+        component: CompanyEdit
+      }
+    ]
+  },
+  {
+    path: '/addCompany',
+    component: CompanyAdd
   }
 ]
 
