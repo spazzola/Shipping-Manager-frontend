76cba3bf3f59b73409dd1df670505253ee0a49df
Rafal Szczotka <rafalszczotka@wp.pl>
2021-04-23 14:49:53 +0200

    Added showing invoice details

---
+++ src/views/invoices/InvoiceDetail.vue
@@ -0,0 +1,182 @@
+<template>
+  <nav-menu></nav-menu>
+  <invoice-menu></invoice-menu>
+  <div class="content" v-if="!isEditMode" style="margin-top: -2rem">
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Numer faktury:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.invoiceNumber }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Nadano w:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.issuedIn }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Data wystawienia:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.issuedDate }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Wystawiona dla:</div>
+      <div class="col-2 col-content">
+        {{ selectedInvoice.receivedBy.companyName }}
+      </div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-6 col-content">
+        NIP: {{ selectedInvoice.receivedBy.nip }}
+      </div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Metoda płatności:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.paymentMethod }}</div>
+    </div>
+
+    <div class=" row-top-margin">
+      <div class="col-2 offset-4 col-title">Sprzedane usługi:</div>
+      <product-info 
+      :products="selectedInvoice.products"
+      :currency="selectedInvoice.currency"></product-info>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Wartość netto:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.valueWithoutTax }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Wartość brutto:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.valueWithTax }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Waluta:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.currency }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Zapłacona kwota:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.paidAmount }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Zostało do zapłaty:</div>
+      <div class="col-2 col-content">{{ selectedInvoice.amountToPay }}</div>
+    </div>
+
+    <div class="row row-top-margin">
+      <div class="col-2 offset-4 col-title">Zapłacona:</div>
+      <div class="col-2 col-content">{{ isPaid }}</div>
+    </div>
+
+    <div class="row row-top-margin row-bottom-margin">
+      <div class="col-2 offset-5">
+        <router-link :to="editInvoice" v-if="!editMode"
+          ><button class="btn btn-sm btn-outline-success" @click="switchMode">
+            Edytuj
+          </button></router-link
+        >
+      </div>
+    </div>
+  </div>
+
+  <router-view v-else style="margin-top: -2rem">
+    <invoice-edit :selectedInvoice="selectedInvoice"> </invoice-edit>
+  </router-view>
+</template>
+
+<script>
+import NavMenu from "../nav/NavMenu.vue";
+import InvoiceMenu from "./InvoiceMenu.vue";
+import InvoiceEdit from "./InvoiceEdit.vue";
+import ProductInfo from "./ProductInfo.vue"
+
+export default {
+  components: {
+    NavMenu,
+    InvoiceMenu,
+    InvoiceEdit,
+    ProductInfo
+  },
+  props: ["id"],
+  data() {
+    return {
+      selectedInvoice: null,
+      editMode: false,
+    };
+  },
+  computed: {
+    isPaid() {
+      if (this.selectedInvoice.paid === true) {
+        return "Tak";
+      } else {
+        return "Nie";
+      }
+    },
+    isEditMode() {
+      return this.editMode;
+    },
+    editInvoice() {
+      return this.$route.path + "/edit";
+    },
+  },
+  methods: {
+    switchMode() {
+      this.editMode = !this.editMode;
+    },
+  },
+  created() {
+    this.selectedInvoice = this.$store.getters["invoices/getAllInvoices"].find(
+      (invoice) => invoice.id === parseInt(this.id)
+    );
+    console.log(this.selectedInvoice);
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  width: 100%;
+}
+
+.btn-outline-success {
+  border-color: #3ec1d3;
+  color: #3ec1d3;
+}
+
+.btn-outline-success:hover {
+  background-color: #ff9a00;
+  color: #f6f7d7;
+  border-color: #ff9a00;
+}
+
+.col-title {
+  text-align: right;
+  font-weight: bold;
+}
+
+.col-content {
+  text-align: left !important;
+}
+
+.row-top-margin {
+  margin-top: 1%;
+}
+
+.row-bottom-margin {
+  margin-bottom: 3rem;
+}
+
+p {
+  text-align: left;
+  margin-bottom: 0;
+  font-weight: bold;
+}
+
+.btn {
+  margin-left: 5%;
+}
+</style>
---
+++ src/views/invoices/ProductInfo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-for="(product, index) in products" :key="index" class="row top-margin">
+    <div class="col-2 offset-5 col-title">
+      <p>Nazwa usługi:</p>
+      {{ product.productName }}
+    </div>
+    <div class="col-2 offset-5 col-title">
+      <p>Ilość:</p>
+      {{ product.quantity }}
+    </div>
+    <div class="col-2 offset-5 col-title">
+      <p>Jednostka:</p>
+      {{ product.measureUnit }}
+    </div>
+    <div class="col-2 offset-5 col-title">
+      <p>Cena jednostkowa bez VAT:</p>
+      {{ product.priceWithoutTax }} {{ toUpperCase(currency) }}
+    </div>
+    <div class="col-2 offset-5 col-title">
+      <p>Wartość podatku:</p>
+      {{ product.taxValue * 100 }}%
+    </div>
+    <div class="col-2 offset-5 col-title">
+      <p>Wartość netto:</p>
+      {{ product.valueWithoutTax }} {{ toUpperCase(currency) }}
+    </div>
+    <div class="col-2 offset-5 col-title">
+      <p>Wartość brutto:</p>
+      {{ product.valueWithTax }} {{ toUpperCase(currency) }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["products", "currency"],
+  methods: {
+      toUpperCase(value) {
+          return value.toUpperCase();
+      }
+  }
+};
+</script>
+
+<style scoped>
+p {
+  display: inline;
+  font-weight: bold;
+}
+
+div {
+    text-align: left;
+}
+
+.top-margin {
+    margin-top: 1%;
+}
+</style>
