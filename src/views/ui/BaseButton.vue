02b05812dff9866df0d70ce762ad70c423bc2c44
Rafal Szczotka <rafalszczotka@wp.pl>
2021-05-14 20:30:23 +0200

    Added BaseButton and base BaseAlert

---
+++ src/views/ui/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    :class="buttonClass"
+  >
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+    props: ["buttonType", "buttonText"],
+    computed: {
+        buttonClass() {
+            if (this.buttonType === 'confirm') {
+                return "btn btn-sm btn-outline-success";
+            }
+            if (this.buttonType === 'reject') {
+                return "btn btn-sm btn-outline-danger";
+            }
+            return "btn btn-sm";
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    margin: 0.5rem;
+}
+
+.btn-outline-success {
+  border-color: rgb(7, 150, 7);
+  color: green;
+}
+
+.btn-outline-success:hover {
+  background-color: rgb(7, 156, 7);
+  color: #f6f7d7;
+  border-color: rgb(7, 150, 7);
+}
+</style>
